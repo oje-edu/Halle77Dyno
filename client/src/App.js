c2ad41ecfa37a26dd770ae5d6e8463309886b028
oje-edu <thorsten.oje@gfn.education>
2022-02-21 20:04:03 +0100

    added client and want to display a nIce OSM map with a REACT App .. this step .. OMG .. what is wrong with you devs? .. every 2 month a mayor release and have to learn it ? ... hey i have a lot more to do! ..I DONT NEED a new bachelor ever half year to be a slave of you. Sorry maybe some is my fail .. but now i am fell better .. THY .. *looooooooooooL*

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import BasicMap from "./pages/Leaflet/basic";
+import "./pages/Leaflet/index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <BasicMap />
+      </header>
+    </div>
+  );
+}
+
+export default App;
