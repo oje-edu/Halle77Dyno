b2b4c3c41bb57e12c2436c55ca06bb1438aa6dc4
oje-edu <thorsten.oje@gfn.education>
2022-02-22 19:19:13 +0100

    added episode 12

--- client/src/App.js
+++ client/src/App.js
@@ -16,11 +16,6 @@ export default function App() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {/* <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker> */}
         {plates.plates.map((plate) => (
           <Marker
             key={plate.kz}
