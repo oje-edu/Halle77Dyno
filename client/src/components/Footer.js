93cf8119b8a38b277b36161b35f0e6f456bd9107
oje-edu <thorsten.oje@gfn.education>
2022-02-22 17:35:29 +0100

    header and footer halle77 colors

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { AiFillGithub } from "react-icons/ai";
 
 const Footer = () => {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      Help{" "}
+      <a
+        href="https://github.com/oje-edu/Halle77Dyno"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <AiFillGithub />
+      </a>{" "}
+      welcome
+    </div>
+  );
 };
 
 export default Footer;
