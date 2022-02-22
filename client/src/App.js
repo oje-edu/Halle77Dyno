f91de1004f87d435bbcfd1af8ed09f6dd065f1f9
oje-edu <thorsten.oje@gfn.education>
2022-02-22 08:19:08 +0100

    added client

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { Icon } from "leaflet";
+import plates from "./data/plates.json";
+import "./App.css";
+
+const position = [51.5072, 7.49142];
+
+export default function App() {
+  return (
+    <MapContainer center={position} zoom={6}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {/* <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker> */}
+      {plates.plates.map((plate) => (
+        <Marker
+          key={plate.kz}
+          position={[
+            plate.geometry.coordinates[0],
+            plate.geometry.coordinates[1],
+          ]}
+        />
+      ))}
+    </MapContainer>
+  );
+}
