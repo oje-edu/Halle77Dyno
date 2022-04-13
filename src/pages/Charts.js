e6a3cab8fcd9f74769de894b7ebe8d05bfc85f9c
oje-edu <thorsten.oje@gfn.education>
2022-04-13 08:17:50 +0200

    ps chart

--- src/pages/Charts.js
+++ src/pages/Charts.js
@@ -1,10 +1,16 @@
 import { useState, useEffect } from "react";
 // import _ from "lodash";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-import { Doughnut } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend,
+  RadialLinearScale,
+} from "chart.js";
+import { Doughnut, PolarArea } from "react-chartjs-2";
 import axios from "../api/axios";
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(RadialLinearScale, ArcElement, Tooltip, Legend);
 
 const CARS_URL = "/cars";
 
@@ -28,7 +34,19 @@ const Charts = () => {
     type: mediaType,
     count: data?.filter((item) => item.brand === mediaType).length,
   }));
+
+  let ps = 0;
+  let ps1 = 0;
+  let ps2 = 0;
+
+  data.forEach((car) => {
+    ps += car.hp;
+    ps1 += Math.round(car.ps1);
+    ps2 += Math.round(car.ps2);
+  });
+
   // console.log(counts);
+  // console.log(ps1 + "PS")
   // const sortedCounts = _.orderBy(counts, (r) => r.count, ["desc"]);
 
   const chartData = {
@@ -37,28 +55,43 @@ const Charts = () => {
       {
         data: counts?.map((item) => item.count),
         backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
+          "rgba(220, 38, 38,0.5)",
+          "rgba(101, 163, 13,0.5)",
+          "rgba(153, 246, 228,0.5)",
+          "rgba(34, 211, 238,0.5)",
+          "rgba(162, 28, 175,0.5)",
+          "rgba(236, 72, 153,0.5)",
         ],
+        borderWidth: 0,
       },
     ],
-    responsive: true
+    responsive: true,
   };
 
-  console.log(chartData)
+  const psData = {
+    labels: ["PS lt. Hersteller", "PS Messung 1", "PS Messung 2"],
+    datasets: [
+      {
+        data: [ps, ps1, ps2],
+        backgroundColor: [
+          "rgba(37, 150, 190, 0.5)",
+          "rgba(14, 116, 144, 0.5)",
+          "rgba(162, 28, 175, 0.5)",
+        ],
+        borderWidth: 0,
+      },
+    ],
+    responsive: true,
+  };
 
+  // console.log(psData)
 
   return (
     <main className="bg-secondary">
       <h1 className="pt-2 text-4xl font-bold text-center text-primary-dark">
         Statistiken
       </h1>
-      <div className="py-2 mx-auto rounded md:w-2/5 bg-primary-dark">
-        <h2 className="text-center text-secondary">Marken</h2>
+      <div className="mx-auto rounded md:w-2/5 ">
         <div className="flex flex-col px-2">
           {/* <ul>
           {sortedCounts?.map((count) => (
@@ -68,7 +101,16 @@ const Charts = () => {
             </li>
           ))}
         </ul> */}
-          <Doughnut data={chartData} />
+          <div className="py-2 mb-2 rounded bg-primary-dark">
+            <h2 className="text-center text-secondary">Marken</h2>
+            <Doughnut data={chartData} />
+          </div>
+          <div className="py-2 rounded bg-primary-dark">
+            <h2 className="mt-4 text-center text-secondary">
+              Gemessene PS (gesamt)
+            </h2>
+            <PolarArea data={psData} />
+          </div>
         </div>
       </div>
     </main>
